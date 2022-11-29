7d2c6ff12cd23f82c4702e453a87dbb1beec60d1
nickmakha <107730817+nickmakha@users.noreply.github.com>
2022-11-30 01:18:18 +0400

    Add files via upload

---
+++ HomeWork1.java
@@ -0,0 +1,51 @@
+// 1) Создать по одной переменной каждого типа данных, который ты знаешь.
+// 2) Присвоить каждой переменной значение в диапазоне типа.
+// 3) Вывести значение каждой переменной в консоль, каждое значение выводить с новой строки.
+
+
+//public class HomeWork1 {
+//    public static void main(String[] args) {
+//        int number = 20;
+//        byte b = 125;
+//        short s = 185;
+//        char c = 'c';
+//        char ch = 50;
+//        long l = 9832;
+//        boolean bool = true;
+//        float f = 26.7F;
+//        double d = 23.89561458;
+//        System.out.println(number);
+//        System.out.println(b);
+//        System.out.println(s);
+//        System.out.println(c);
+//        System.out.println(ch);
+//        System.out.println(l);
+//        System.out.println(bool);
+//        System.out.println(f);
+//        System.out.println(d);
+//    }
+
+
+//1) Создать три переменных (number1, number2, number3) типа int, присвоить им значение 10, 18, 21 соответственно.
+//2) Не используя чисел, сделать так, чтобы значение у переменной number1 изменилось на 18
+//3) Не используя чисел, сделать так, чтобы значение у переменной number2 изменилось на 21
+//4) Не используя чисел, сделать так, чтобы значение у переменной number3 изменилось на 10
+//5) Вывести значение каждой переменной в консоль, каждое значение выводить с новой строки.
+
+//    public static void main(String[] args) {
+//        int number1 = 10;
+//        int number2 = 18;
+//        int number3 = 21;
+//        int temp = number1;         //нужно создать временную вспомогательную переменную temp
+//
+//        number1 = number2;
+//        number2 = number3;
+//        number3 = temp;
+//        System.out.println(number1);
+//        System.out.println(number2);
+//        System.out.println(number3);
+//    }
+//
+//
+//}
+
